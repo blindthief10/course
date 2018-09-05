f88a01281204d958316cc12b84b23247df40c838
blindthief10 <kostasdiakogiannis23@gmail.com>
2018-09-05 12:46:47 +0200

    milad almost destroyed the session, but i managed to bring it back with heroics

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 var money = 500;
 var cheapestRoom = 200;
 var isVip = true;
-var customersColor = 'orange';
+var customersColor = 'blue';
 
 // money -= 400;
 var enoughMoney = money > cheapestRoom;
@@ -10,14 +10,18 @@ if (enoughMoney) {
 
   if (money >= cheapestRoom * 2) {
       if (isVip) {
-        if (customersColor === 'red') {
-          console.log('Take the red suite');
-        } else if (customersColor === 'blue') {
-          console.log('Take the blue suite');
-        } else if (customersColor === 'green') {
-          console.log('Take the green');
-        } else {
-          console.log('Take the grey');
+        switch (customersColor){
+          case 'red':
+            console.log('take the red suite!');
+            break;
+          case 'blue':
+            console.log('take the blue suite!');
+            break;
+          case 'green':
+            console.log('take the green suite!');
+            break;
+          default:
+            console.log('take the Gray suite!');
         }
       } else {
         console.log('Get the bigger room!');
