284fb7a047c62d56dbcc321e76fcb226050ebded
blindthief10 <kostasdiakogiannis23@gmail.com>
2018-09-10 10:35:49 +0200

    add if statements exercises

--- scripts/index.js
+++ scripts/index.js
@@ -1,98 +1,61 @@
-// first exercise
+// First exercise
 
 var testedNum = 5;
+var message = ``;
 
 if (testedNum > 0) {
-  console.log('It is a positive number!');
+  message += `This is a positive number`;
 } else if (testedNum < 0) {
-  console.log('It is a negative number');
+  message += `This is a negative number`;
 } else {
-  console.log('It is exactly 0!');
+  message += `This is zero!`;
 }
 
-// second exercise
+message += ' And i am very happy about it!';
 
-var secondNumber = 2;
+console.log(message);
 
-if (secondNumber !== 0) {
-  console.log(- secondNumber);
+// Second exercise
+
+var givenNumber = -5;
+
+givenNumber !== 0 ? console.log(-1 * givenNumber) : console.log(0);
+
+// Fourth exercise
+
+var firstNum = 50,
+    secondNum = 50,
+    thirdNum = 35,
+    biggestNum;
+
+if (firstNum > secondNum && firstNum > thirdNum) {
+  biggestNum = firstNum;
+} else if (secondNum > firstNum && secondNum > thirdNum) {
+  biggestNum = secondNum;
+} else if (thirdNum > firstNum && thirdNum > secondNum){
+  biggestNum = thirdNum;
 } else {
-  console.log(secondNumber);
+  biggestNum = `Officially we have a tie!`;
 }
 
-// Third exercise
-var math = 12;
-var literature = 14;
-var programming = 9;
-var historie = 14;
-var geography = 19;
+console.log(biggestNum);
 
-var summary = math + literature + programming + historie + geography;
-var average = summary / 5;
+// Exercise leap year
 
-console.log(summary);
-console.log(average);
+var givenYear = 2008;
+var leapYearMsg = `Yes! ${givenYear} is a leap year!`;
+var noLeapHere = `Nope! ${givenYear} is not a leap year!`;
 
-// Print the bigger number of three
+givenYear % 4 === 0 ? console.log(leapYearMsg) : console.log(noLeapHere);
 
-var testNum = 22;
-var testNumSecond = 35;
-var testNumThird = 19;
+// Months exercise
 
-if (testNum > testNumSecond && testNum > testNumThird) {
-  console.log(`The biggest number of three is ${testNum}`);
-} else if (testNumSecond > testNum && testNumSecond > testNumThird) {
-  console.log(`The biggest number of three is ${testNumSecond}`);
-} else if (testNumThird > testNum && testNumThird > testNumSecond) {
-  console.log( `The biggest number of three is ${testNumThird}`);
-} else {
-  console.log(`There is no specific number that is bigger! We have a tie!`);
+var numMonth = 6;
+
+if (numMonth === 2) {
+  console.log(`The february has 28 days you dumm!`);
+} else if (numMonth === 4 || numMonth === 6 || numMonth === 9 || numMonth === 11) {
+  console.log(`The month you entered contains 30 days`);
+}else {
+  console.log('The month you entered contains 31 days!');
 }
-
-//  Months exercise
-
-var monthNumber = 11;
-var februaryMsg = 'February has always 28 days you dumm!';
-var thirties = 'The month you selected contains 30 days!';
-var thirtyOnes = 'The month you selected contains 31 days!'
-
-if (monthNumber < 1 && monthNumber > 12) {
-  console.log('Month should be represented as integer between 1 and 12');
-} else if (monthNumber <= 7) {
-
-    if (monthNumber % 2 === 0) {
-
-        if (monthNumber === 2) {
-          console.log(februaryMsg);
-        } else {
-          console.log(thirties);
-        }
-
-    } else {
-        console.log(thirtyOnes);
-    }
-
-} else {
-
-    if (monthNumber % 2 === 0) {
-      console.log(thirtyOnes)
-    } else {
-      console.log(thirties);
-    }
-}
-
-//  Leap year exercise
-
-var year = 2000;
-var leapYearMsg = `Yep! ${year} is a leap year!`;
-var noLeapYearMsg = `Nope! ${year} is no leap year!`;
-
-year % 4 === 0 ? console.log(leapYearMsg) : console.log(noLeapYearMsg);
-
-// Addition without loop exercise
-
-var startNum = 1;
-var endNum = 100;
-
-var sumCalculation = (startNum + endNum) * endNum / 2;
-console.log(sumCalculation);
