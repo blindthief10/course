6b059b22b290ae8bf20fd9498048568fe04296a7
blindthief10 <kostasdiakogiannis23@gmail.com>
2018-09-20 11:59:52 +0200

    bank restored

--- scripts/bankproblem.js
+++ scripts/bankproblem.js
@@ -3,44 +3,30 @@ let males = {
   hobby: 'Reading'
 }
 
-let jake = Object.create(males);
-let gidday = Object.create(males);
-let steffan = Object.create(males);
+jake = {
+  firstName: 'Konstantinos',
+  lastName: 'Diakogiannis',
+  bankAccountInAmericanDollars: 1000,
+  countryOfResidence: 'Greece',
+  convertToLocal: 0.86
+}
 
-console.log(jake.hobby);
+let gidday = {
+  firstName: 'Meuron',
+  lastName: 'Gday',
+  bankAccountInAmericanDollars: 2000,
+  countryOfResidence: 'Australia',
+  convertToLocal: 1.38
+}
 
-// jake = {
-//   firstName: 'Konstantinos',
-//   lastName: 'Diakogiannis',
-//   bankAccountInAmericanDollars: 1000,
-//   countryOfResidence: 'Greece',
-//   convertToLocal: 0.86
-// }
-
-// let gidday = {
-//   firstName: 'Meuron',
-//   lastName: 'Gday',
-//   bankAccountInAmericanDollars: 2000,
-//   countryOfResidence: 'Australia',
-//   convertToLocal: 1.38
-// }
-
-// let steffan = {
-//   firstName: 'Steffan',
-//   lastName: 'Effenberg',
-//   bankAccountInAmericanDollars: 5000,
-//   countryOfResidence: 'Switzerland',
-//   convertToLocal: 0.96
-// }
+let steffan = {
+  firstName: 'Steffan',
+  lastName: 'Effenberg',
+  bankAccountInAmericanDollars: 5000,
+  countryOfResidence: 'Switzerland',
+  convertToLocal: 0.96
+}
 
 let convertToLocalCurrency = function(person) {
   person.bankAccountInLocalCurrency = person.bankAccountInAmericanDollars * person.convertToLocal;
 }
-
-// convertToLocalCurrency(jake);
-// convertToLocalCurrency(gidday);
-// convertToLocalCurrency(steffan);
-//
-// console.log(jake);
-// console.log(gidday);
-// console.log(steffan);
