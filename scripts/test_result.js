b91b0474049518d6b8c379b2af77907b393848a6
blindthief10 <kostasdiakogiannis23@gmail.com>
2018-09-25 14:38:05 +0200

    push method

---
+++ scripts/test_result.js
@@ -0,0 +1,22 @@
+let testResults = [78, 82, 45, 69, 23, 91, 55, 39, 66, 60, 45, 72];
+testResults.push(66);
+testResults.push(25);
+console.log(testResults);
+console.log(testResults.length);
+
+
+let calculateFailurePercentage = function(collection, basis) {
+  let failedCollection = [];
+
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i] < basis) {
+      failedCollection.push(collection[i]);
+    }
+  }
+
+  let result = (failedCollection.length / collection.length) * 100;
+
+  return `The percentage of failed students is ${result}% of total participants`;
+}
+
+console.log(calculateFailurePercentage(testResults, 50));
