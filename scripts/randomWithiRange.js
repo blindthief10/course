5bbf1635aae685bb8368ed390cfd162b813ca46c
blindthief10 <kostasdiakogiannis23@gmail.com>
2018-10-10 16:00:58 +0200

    date future event

--- scripts/randomWithiRange.js
+++ scripts/randomWithiRange.js
@@ -1,8 +1,13 @@
+let beginDate = Date.now();
+
 let createNumWithinRange = function(lowNumber, highNumber) {
   let range = Math.abs(lowNumber - highNumber) + 1;
   let randomNumber = Math.floor(Math.random() * range + lowNumber);
-  console.log(randomNumber);
 }
 
-createNumWithinRange(1, 10);
+for (let i = 0; i <= 10000; i++) {
 createNumWithinRange(50, 55);
+}
+
+let endDate = Date.now();
+console.log(`This script took ${endDate - beginDate} ms to execute`);
