79f904585d8dc2b1f37f2dae90b33444f812f647
blindthief10 <kostasdiakogiannis23@gmail.com>
2018-10-18 10:40:12 +0200

    create toggle paragraph height slider

--- exercises/createToggle/createToggle.js
+++ exercises/createToggle/createToggle.js
@@ -1,9 +1,11 @@
 let button = document.querySelector('button');
 let list = document.querySelector('ul');
-let items = document.querySelectorAll('ul li');
-list.style.width = '25.1vw';
+let paragraph = document.querySelector('p');
+let secondButton = document.querySelector('button:nth-of-type(2)');
+let paragraphHeightFullScreen = paragraph.clientHeight;
 
 let listVisible = false;
+let paragraphIsShown = true;
 
 button.addEventListener('click', function() {
   if (listVisible) {
@@ -14,4 +16,17 @@ button.addEventListener('click', function() {
     button.innerHTML = 'Get them out of here!';
   }
   listVisible = !listVisible;
+})
+
+secondButton.addEventListener('click', function() {
+  if (paragraphIsShown) {
+    paragraph.style.height = '0';
+    paragraph.style.padding = '0';
+    secondButton.innerHTML = 'Show Paragraph';
+  } else {
+    paragraph.style.padding = '1vw 4vw';
+    paragraph.style.height = paragraphHeightFullScreen + 'px';
+    secondButton.innerHTML = 'Hide Paragraph';
+  }
+  paragraphIsShown = !paragraphIsShown;
 })
---
+++ exercises/createToggle/tester.js
@@ -0,0 +1,15 @@
+let myAge = 15;
+let acceptedAge = 18;
+
+if (myAge === 26) {
+  console.log('Sweet 26');
+} else if (myAge >= acceptedAge) {
+  console.log('Du bust erwachsene aber nicht 26!');
+}else if (myAge < 18 && myAge >= 16) {
+  console.log('Du bist ein teenager!');
+}else {
+  console.log('Du bist klein in einem Club zu gehen!');
+}
+
+
+console.log('Good morning');
