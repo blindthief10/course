c2489ab5d589ec993e63cee6be04065d19b960f1
blindthief10 <kostasdiakogiannis23@gmail.com>
2018-11-07 10:55:49 +0100

    dom loaded event, remove elements

--- theoryDom/theoryDom.js
+++ theoryDom/theoryDom.js
@@ -1,40 +1,47 @@
-let input = document.querySelector('input');
-let button = document.querySelector('button');
-let article = document.querySelector('article');
-let checkbox = document.querySelector('input[type="checkbox"]');
-let paragraphs = document.querySelectorAll('p');
-let image = document.querySelector('img');
-let selectFields = document.querySelectorAll('select');
-let link = document.querySelector('a');
-let textarea = document.querySelector('textarea');
+document.addEventListener('DOMContentLoaded', function(ev) {
 
-input.addEventListener('keyup', function(ev) {
-  let val = input.value;
-  val ? article.innerText = val : article.innerText = input.placeholder
-})
+  let input = document.querySelector('input');
+  let button = document.querySelector('button');
+  let article = document.querySelector('article');
+  let checkbox = document.querySelector('input[type="checkbox"]');
+  let image = document.querySelector('img');
+  let selectFields = document.querySelectorAll('select');
+  let link = document.querySelector('a');
+  let textarea = document.querySelector('textarea');
+  let paragraphs = document.querySelectorAll('p');
 
-checkbox.addEventListener('click', function(ev) {
-  checkbox.checked ? input.type = 'password' : input.type = 'text'
-  console.log(input.value);
-})
+  input.addEventListener('keyup', function(ev) {
+    let val = input.value;
+    val ? article.innerText = val : article.innerText = input.placeholder
+  })
 
-selectFields[0].addEventListener('change', function(ev) {
-  let selectedValue = selectFields[0].value;
-  image.src = `${selectedValue}.jpg`;
-})
+  checkbox.addEventListener('click', function(ev) {
+    checkbox.checked ? input.type = 'password' : input.type = 'text'
+    console.log(input.value);
+  })
 
-
-selectFields[1].addEventListener('change', function(ev) {
-  link.innerHTML = `Go to <span>${ev.currentTarget.value}</span>`;
-  link.href =  `https://www.${ev.currentTarget.value}.com`;
-})
+  selectFields[0].addEventListener('change', function(ev) {
+    let selectedValue = selectFields[0].value;
+    image.src = `../images/${selectedValue}.jpg`;
+  })
 
 
+  selectFields[1].addEventListener('change', function(ev) {
+    link.innerHTML = `Go to <span>${ev.currentTarget.value}</span>`;
+    link.href =  `https://www.${ev.currentTarget.value}.com`;
+    console.log(ev.currentTarget);
+  })
 
-button.addEventListener('click', function(ev) {
-  let text = textarea.value;
-  let newParagraph = document.createElement('P');
-  newParagraph.innerText = text;
-  document.body.appendChild(newParagraph);
-  textarea.value = '';
+  button.addEventListener('click', function(ev) {
+    let text = textarea.value;
+    let newParagraph = document.createElement('P');
+    newParagraph.innerText = text;
+    document.body.appendChild(newParagraph);
+    textarea.value = '';
+    
+    newParagraph.addEventListener('click', function(ev) {
+      document.body.removeChild(ev.currentTarget);
+    })
+  })
+
 })
