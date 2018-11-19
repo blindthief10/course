d6352b793ab6198ac0dcbdd78913ecc4bef538bc
blindthief10 <kostasdiakogiannis23@gmail.com>
2018-11-19 16:58:03 +0100

    textarea solution

--- regex/regex.js
+++ regex/regex.js
@@ -64,8 +64,8 @@ let urlRegex = /^https?:\/{2}w{3}\.[\w\-]{2,50}\.[a-z]{2,4}$/i;
 // console.log(urlRegex.test(thirdUrl));
 // console.log(urlRegex.test(fourthUrl));
 
-let textQuote = 'Pussycat dolls';
+let bear = 'bear, I love watching bears fishing salmon in Kamtchaka. My morning routine is to shave my beard. The heat is unbearable';
 
-let boundaryRegex = /cat\b/i;
+let bearRegex = /\bbears?\b/ig
 
-console.log(boundaryRegex.test(textQuote));
+console.log(bear.match(bearRegex));
