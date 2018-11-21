375a7b01045a88d09f61555203c9c0b8ddebed6b
blindthief10 <kostasdiakogiannis23@gmail.com>
2018-11-21 15:51:21 +0100

    array methods, maps, foreach, sets, includes

--- es6Theory/es6Theory.js
+++ es6Theory/es6Theory.js
@@ -7,19 +7,22 @@ btn.addEventListener('click', ev => {
 // Ecma Script 6
 
 // Default values on function parameters
-var name = 'jake';
-// Function expression. Immediately invoked functions
-let factorial = function(nameToChange) {
-  name = nameToChange;
-
+let printName = function(name = 'jake') {
+  console.log(name);
 }
 
-// function declarations
-function whatever(whatever) {
-  return whatever;
+// printName('Mauro'); // Prints Mauro
+// printName('Daniel'); // Prints Daniel
+// printName(); // Prints jake
+
+let printNameArrowStyle = name => {
+  console.log(name);
 }
 
 let squareRoot = (num = 9) => Math.sqrt(num)
+
+// console.log(squareRoot(15)); // Prints the square root of 15
+// console.log(squareRoot());  // Prints the square root of 9
 
 let findMultiples = (first, second) => {
   let answer;
@@ -27,9 +30,9 @@ let findMultiples = (first, second) => {
   return answer;
 }
 
-console.log(findMultiples(9, 3));
-console.log(findMultiples(80, 2));
-console.log(findMultiples(25, 3));
+// console.log(findMultiples(9, 3));
+// console.log(findMultiples(80, 2));
+// console.log(findMultiples(25, 3));
 
 let telephoneData = [
   {countryCode: '0030', country: 'Greece'},
@@ -43,3 +46,89 @@ let telephoneData = [
   {countryCode: '0031', country: 'Holland'},
   {countryCode: '0049', country: 'Germany'},
 ]
+
+let returnCountryFromCode = code => {
+  return telephoneData.find(item => item.country === code).countryCode;
+}
+
+// console.log(returnCountryFromCode('Holland'));
+// console.log(returnCountryFromCode('Belgium'));
+// console.log(returnCountryFromCode('Syria'));
+
+let airlinePrices = [
+  {airlinesName: 'Lufthansa', avgPriceTicket: 85.6},
+  {airlinesName: 'Aegean', avgPriceTicket: 65.8},
+  {airlinesName: 'KLM', avgPriceTicket: 102.82},
+  {airlinesName: 'British Airways', avgPriceTicket: 114.9},
+  {airlinesName: 'Iberia', avgPriceTicket: 77.2},
+  {airlinesName: 'TAP Portugal', avgPriceTicket: 58.9},
+  {airlinesName: 'Alitalia', avgPriceTicket: 84.2},
+  {airlinesName: 'Air France', avgPriceTicket: 105.6},
+  {airlinesName: 'Singapore Airlines', avgPriceTicket: 119.2},
+  {airlinesName: 'Emirates', avgPriceTicket: 135.4},
+]
+
+let airFreightCompanies = [
+  {airlinesName: 'Lufthansa', avgPriceTicket: 85.6},
+  {airlinesName: 'Aegean', avgPriceTicket: 65.8},
+  {airlinesName: 'KLM', avgPriceTicket: 102.82},
+  {airlinesName: 'British Airways', avgPriceTicket: 114.9},
+  {airlinesName: 'Iberia', avgPriceTicket: 77.2},
+  {airlinesName: 'TAP Portugal', avgPriceTicket: 58.9},
+  {airlinesName: 'Alitalia', avgPriceTicket: 84.2},
+  {airlinesName: 'Air France', avgPriceTicket: 105.6},
+  {airlinesName: 'Singapore Airlines', avgPriceTicket: 119.2},
+  {airlinesName: 'Emirates', avgPriceTicket: 135.4},
+]
+
+let cheapAirlines = (threshold = 100) => {
+  airFreightCompanies
+  .filter(airFreight => airFreight.avgPriceTicket < threshold)
+  .forEach(company => {
+    console.log(company.airlinesName);
+  })
+}
+
+cheapAirlines();
+
+let names = [
+  'Mauro',
+  'Eugen',
+  'Mauro',
+  'Meir',
+  'Eugen',
+  'Jens',
+  'Jens',
+  'Jake',
+  'Mohammed',
+  'Mauro',
+  'Mohammed',
+  'Marcelo',
+  'Sue',
+  'Murhaf',
+  'Jens'
+];
+
+let createUniqueNames = (collection = names) => {
+  let uniqueNames = [];
+
+  for (let name of collection) {
+    if (!uniqueNames.includes(name)) {
+      uniqueNames.push(name);
+    }
+
+  }
+
+  return uniqueNames;
+}
+
+let uniqueNames = createUniqueNames();
+console.log(uniqueNames);
+
+let specialNames = new Set();
+
+for (let name of names) {
+  specialNames.add(name);
+}
+
+console.log(specialNames);
