497368c6870030305ab7beb756049590b304f198
blindthief10 <kostasdiakogiannis23@gmail.com>
2018-11-25 16:30:39 +0100

    reduce function, class tigers

--- es6Theory/es6Theory.js
+++ es6Theory/es6Theory.js
@@ -24,32 +24,8 @@ class SiberianTiger extends Tiger {
 let richardParker = new BengalTiger('Richard Parker', 12);
 let joe = new SiberianTiger('Joe', 18);
 
+let returnSum = (array) => {
+  return array.reduce((a, b) => a + b, 0);
+}
 
-// let name = 'Jake';
-//
-// setTimeout(function() {
-//   name = 'Daniel';
-//   console.log(name);
-//   setTimeout(function() {
-//     name = 'Eugen';
-//     console.log(name);
-//     setTimeout(function() {
-//       name = 'Meir';
-//       console.log(name);
-//       setTimeout(function() {
-//         name = 'Sue';
-//         console.log(name);
-//       }, 1000)
-//     }, 2000)
-//   }, 2000)
-// }, 3000)
-//
-// console.log(name);
-
-
-
-// let returnSum = (array) => {
-//   return array.reduce((a, b) => a + b, 0);
-// }
-//
-// console.log(returnSum([0, 1, 2, 5, 7, 3])); // Returns 18 as a total Value of sum
+console.log(returnSum([0, 1, 2, 5, 7, 3])); // Returns 18 as a total Value of sum
