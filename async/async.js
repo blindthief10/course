2ea9a1b81b0fb448bfc5caff484ce709bd1d2534
blindthief10 <kostasdiakogiannis23@gmail.com>
2018-11-26 15:52:01 +0100

    add reject, all rulers with one then

--- async/async.js
+++ async/async.js
@@ -1,34 +1,43 @@
 let currentRuler = 'Cersei Lannister';
 
-let changeRulerTo = (firstRuler, secondRuler, thirdRuler, fourthRuler, finalRuler) => {
-  setTimeout(() => {
-    currentRuler = firstRuler;
-    console.log(currentRuler);
-    setTimeout(() => {
-      currentRuler = secondRuler;
-      console.log(currentRuler);
+let changeRuler = name => {
+  currentRuler = name;
+  return currentRuler;
+}
+
+let asyncChangeRuler = (name, time) => {
+  let promisedName = new Promise((resolve, reject) => {
+    if (name === 'Jon Snow') {
+      reject('Bastards are not allowed to sit on the iron throne!');
+    }
       setTimeout(() => {
-        currentRuler = thirdRuler;
+        resolve(name);
+      }, time);
+  })
+  return promisedName;
+}
+
+let rulers = ['Danny', 'Jon Snow', 'Euron Greyjoy', 'Jamie Lannister', 'NightKing'];
+let times = [5000, 3000, 2000, 1500, 1000]
+let counter = 0;
+
+let makeRulersAtOnce = (counter) => {
+  if (counter === rulers.length) {
+    return;
+  }
+
+  asyncChangeRuler(rulers[counter], times[counter])
+      .then(name => {
+        currentRuler = name;
         console.log(currentRuler);
-        setTimeout(() => {
-          currentRuler = fourthRuler;
-          console.log(currentRuler);
-          setTimeout(() => {
-            currentRuler = finalRuler;
-            console.log(currentRuler);
-          }, 1000)
-        }, 1500)
-      }, 2000)
-    }, 3000)
-  }, 5000)
+        counter++;
+        makeRulersAtOnce(counter);
+      })
+      .catch(errorMessage => {
+        console.warn(errorMessage);
+      })
 }
 
-console.log(currentRuler);
-changeRulerTo('Danny', 'Jon Snow', 'Euron Greyjoy', 'Jamie Lannister', 'NightKing');
-console.log(currentRuler);
+makeRulersAtOnce(counter);
 
-let executedFunction = () => {
-  console.log('executed!')
-}
-
-setTimeout(executedFunction, 7000);
+console.log(currentRuler);
