999e87c1dd35cc37e1cd783a8ef86ff6e8f1c626
blindthief10 <kostasdiakogiannis23@gmail.com>
2018-12-03 11:26:27 +0100

    changed to async await way

--- async/async.js
+++ async/async.js
@@ -1,3 +1,7 @@
+// Change the function the async await way
+// Accept multiple cities from input, create multiple sections
+// Have a timeout for every new box that is created
+
 const authToken = '16d4785f9c10724266053adb3c29dcfd';
 const btn = document.querySelector('button');
 const form = document.querySelector('form');
@@ -14,39 +18,35 @@ const colorTemperatures = [
   {limitTemp: -100, color: 'white'}
 ];
 
-let getWeatherForCity = ev => {
+let getWeatherForCity = async ev => {
   ev.preventDefault();
   let city = inputField.value;
   let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${authToken}`;
-  fetch(weatherUrl)
-    .then(res => res.json())
-    .then(weatherData => {
-      console.log(weatherData);
-      let section = document.createElement('SECTION');
-      let heading = document.createElement('H3');
-      let tempParagraph = document.createElement('P');
-      let desc = document.createElement('SPAN');
-      let littleImage = document.createElement('IMG');
-      let footer = document.createElement('FOOTER');
-      let tempInCelsius = Math.round(weatherData.main.temp - 273);
-      let color = colorTemperatures.find(entry => tempInCelsius > entry.limitTemp).color;
-      heading.innerText = weatherData.name;
-      tempParagraph.innerHTML = `<b>${tempInCelsius}</b> <sup>O</sup>C`;
-      desc.innerText = weatherData.weather[0].description;
-      littleImage.src = `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-      section.appendChild(heading);
-      section.appendChild(tempParagraph);
-      footer.appendChild(desc);
-      footer.appendChild(littleImage);
-      section.appendChild(footer);
-      section.style.background = color;
-      divContainer.appendChild(section);
-      inputField.value = '';
-      inputField.focus();
-    })
-    .catch(errorMsg => {
-      console.error(errorMsg);
-    })
+  let res = await fetch(weatherUrl);
+  let weatherData = await res.json();
+  console.log(weatherData);
+  let section = document.createElement('SECTION');
+  let heading = document.createElement('H3');
+  let tempParagraph = document.createElement('P');
+  let desc = document.createElement('SPAN');
+  let littleImage = document.createElement('IMG');
+  let footer = document.createElement('FOOTER');
+  let tempInCelsius = Math.round(weatherData.main.temp - 273);
+  let color = colorTemperatures.find(entry => tempInCelsius > entry.limitTemp).color;
+  heading.innerText = weatherData.name;
+  tempParagraph.innerHTML = `<b>${tempInCelsius}</b> <sup>O</sup>C`;
+  desc.innerText = weatherData.weather[0].description;
+  littleImage.src = `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  section.appendChild(heading);
+  section.appendChild(tempParagraph);
+  footer.appendChild(desc);
+  footer.appendChild(littleImage);
+  section.appendChild(footer);
+  section.style.background = color;
+  divContainer.appendChild(section);
+  inputField.value = '';
+  inputField.focus();
+
 }
 
 form.addEventListener('submit', getWeatherForCity);
