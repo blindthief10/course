bf70dbc9232296d7ebbe13d34d837108e67ef94c
blindthief10 <kostasdiakogiannis23@gmail.com>
2018-12-03 11:32:30 +0100

    multiple requests added

--- async/async.js
+++ async/async.js
@@ -1,4 +1,3 @@
-// Change the function the async await way
 // Accept multiple cities from input, create multiple sections
 // Have a timeout for every new box that is created
 
@@ -20,32 +19,39 @@ const colorTemperatures = [
 
 let getWeatherForCity = async ev => {
   ev.preventDefault();
-  let city = inputField.value;
-  let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${authToken}`;
-  let res = await fetch(weatherUrl);
-  let weatherData = await res.json();
-  console.log(weatherData);
-  let section = document.createElement('SECTION');
-  let heading = document.createElement('H3');
-  let tempParagraph = document.createElement('P');
-  let desc = document.createElement('SPAN');
-  let littleImage = document.createElement('IMG');
-  let footer = document.createElement('FOOTER');
-  let tempInCelsius = Math.round(weatherData.main.temp - 273);
-  let color = colorTemperatures.find(entry => tempInCelsius > entry.limitTemp).color;
-  heading.innerText = weatherData.name;
-  tempParagraph.innerHTML = `<b>${tempInCelsius}</b> <sup>O</sup>C`;
-  desc.innerText = weatherData.weather[0].description;
-  littleImage.src = `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-  section.appendChild(heading);
-  section.appendChild(tempParagraph);
-  footer.appendChild(desc);
-  footer.appendChild(littleImage);
-  section.appendChild(footer);
-  section.style.background = color;
-  divContainer.appendChild(section);
-  inputField.value = '';
-  inputField.focus();
+  let cities = inputField.value.split(`-`);
+  try {
+    for (city of cities) {
+      let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${authToken}`;
+      let res = await fetch(weatherUrl);
+      let weatherData = await res.json();
+      console.log(weatherData);
+      let section = document.createElement('SECTION');
+      let heading = document.createElement('H3');
+      let tempParagraph = document.createElement('P');
+      let desc = document.createElement('SPAN');
+      let littleImage = document.createElement('IMG');
+      let footer = document.createElement('FOOTER');
+      let tempInCelsius = Math.round(weatherData.main.temp - 273);
+      let color = colorTemperatures.find(entry => tempInCelsius > entry.limitTemp).color;
+      heading.innerText = weatherData.name;
+      tempParagraph.innerHTML = `<b>${tempInCelsius}</b> <sup>O</sup>C`;
+      desc.innerText = weatherData.weather[0].description;
+      littleImage.src = `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+      section.appendChild(heading);
+      section.appendChild(tempParagraph);
+      footer.appendChild(desc);
+      footer.appendChild(littleImage);
+      section.appendChild(footer);
+      section.style.background = color;
+      divContainer.appendChild(section);
+      inputField.value = '';
+      inputField.focus();
+    }
+
+  }catch (e) {
+    console.warn(e);
+  }
 
 }
 
