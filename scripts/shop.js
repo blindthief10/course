ec81b400d85854f89c3696fe3b5a11b3723f472e
blindthief10 <kostasdiakogiannis23@gmail.com>
2019-01-30 17:06:02 +0100

    senior solution for fishing e-shop

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,54 +1,49 @@
-const fishInfo = [
+const allMyFishes = [];
+
+const fishInformation = [
   {species: 'tuna', price: 10},
   {species: 'salmon', price: 50},
-  {species: 'koi', price: 100}
-];
-const basket = [];
-const myFishMarket = [];
+  {species: 'koi', price: 100},
+  {species: 'bacalao', price: 30}
+]
 
-// populate the myFishMarket
+//populate our market
 
-const fillWithFishes = (numOfFishes, collectionMarket) => {
-  for(let i = 1; i <= numOfFishes; i++) {
-    const randomIndex = Math.floor(Math.random() * fishInfo.length); // This returns either 0, 1 or 2
-    collectionMarket.push(fishInfo[randomIndex].species);
+const populateMarket = numOfFishes => {
+
+  for (let i = 1; i <= numOfFishes; i++) {
+    const createRandomIndex = Math.floor(Math.random() * fishInformation.length) // Generate a random number 0, 1 or 2
+    allMyFishes.push(fishInformation[createRandomIndex].species);
+  }
+
+}
+
+populateMarket(100);
+
+const removeFish = (type, numOfRemovedFishes) => {
+  for (let i = 1; i <= numOfRemovedFishes; i++) {
+    allMyFishes.splice(type, 1);
   }
 }
 
-fillWithFishes(100, myFishMarket);
+const createFishOrder = function(numOfTuna, numOfSalmons, numOfKois, numOfBacalaos) {
+  const basket = [];
 
-console.log(myFishMarket);
-
-const addFishToOrder = type => {
-  if (!myFishMarket.includes(type)) {
-    console.log(`Sorry but the ${type} you requested is out of order!`);
-    return false;
-  } else {
-    const selectedFishBasket = basket.find(fish => fish.species === type);
-    if (selectedFishBasket) {
-      selectedFishBasket.quantity++;
-    } else {
-      const findSpecificTypeInfo = fishInfo.find(fish => fish.species === type);
-      basket.push({species: type, price: findSpecificTypeInfo.price, quantity: 1});
-    }
-
-    myFishMarket.splice(type, 1);
+  for(let i = 0; i < fishInformation.length; i++) {
+    const availableTypeFish = allMyFishes.filter(fish => fish === fishInformation[i].species).length;
+    const offeredTypeFish = availableTypeFish >= arguments[i] ? arguments[i] : availableTypeFish
+    removeFish(fishInformation[i].species, offeredTypeFish);
+    basket.push({species: fishInformation[i].species, price: fishInformation[i].price, quantity: offeredTypeFish});
   }
+
+    return basket;
 }
 
-addFishToOrder('salmon');
-addFishToOrder('salmon');
-addFishToOrder('koi');
-addFishToOrder('tuna');
-addFishToOrder('koi');
-addFishToOrder('salmon');
-addFishToOrder('salmon');
-
-console.log(myFishMarket.length);
-console.log(basket);
-
-const calcualateBasketCosts = basket => {
-  return basket.reduce((cost, eachFish) => cost + eachFish.price * eachFish.quantity, 0);
+const calculateCost = (cart) => {
+  return cart.reduce((totalOrderCost, eachFish) => totalOrderCost + eachFish.quantity * eachFish.price, 0);
 }
 
-console.log(calcualateBasketCosts(basket));
+const maurosOrder = createFishOrder(5, 0, 2);
+console.log(calculateCost(maurosOrder));
+
+console.log(maurosOrder);
